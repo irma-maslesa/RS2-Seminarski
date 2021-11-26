cd8a1249a91d5d6debe62e2321f6b1085f28ff59
Irma Masleša <irma.maslesa@edu.fit.ba>
2021-11-26 11:56:11 +0100

    Added Obavijest CRUD Forms

---
+++ Pelikula.WINUI/Forms/Obavijest/FrmObavijestDodajUredi.cs
@@ -0,0 +1,123 @@
+﻿using Pelikula.API;
+using Pelikula.API.Model;
+using Pelikula.API.Model.Obavijest;
+using Pelikula.CORE.Helper.Response;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Pelikula.WINUI.Forms.Obavijest
+{
+    public partial class FrmObavijestDodajUredi : Form
+    {
+        private readonly ApiService _service = new ApiService("Obavijest");
+
+        private readonly int? _id;
+
+        private ObavijestResponse _initial = new ObavijestResponse();
+        private ObavijestUpsertRequest _request = new ObavijestUpsertRequest();
+
+        public FrmObavijestDodajUredi(int? id = null)
+        {
+            _id = id;
+
+            InitializeComponent();
+        }
+
+        private async void FrmObavijestDodajUredi_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+
+            txtTekst.AutoSize = false;
+            txtTekst.WordWrap = true;
+            txtTekst.Multiline = true;
+            txtTekst.ScrollBars = ScrollBars.Vertical;
+
+            Text = "Dodaj artikal";
+
+            if (_id.HasValue)
+            {
+                Text = "Uredi artikal";
+
+                PayloadResponse<ObavijestResponse> response = await _service.GetById<PayloadResponse<ObavijestResponse>>(_id.Value);
+                _initial = response.Payload;
+
+                SetValues();
+            }
+        }
+
+        private void SetValues()
+        {
+            txtNaslov.Text = _initial.Naslov;
+            txtTekst.Text = _initial.Tekst;
+        }
+
+        private async void BtnSpremi_Click(object sender, EventArgs e)
+        {
+            if (ValidateChildren())
+            {
+                _request.Naslov = txtNaslov.Text;
+                _request.Tekst = txtTekst.Text;
+                _request.Datum = _initial.Datum;
+
+                if(_initial.Korisnik != null)
+                    _request.KorisnikId = _initial.Korisnik.Id;
+
+                if (_id.HasValue)
+                {
+
+                    await _service.Update<PayloadResponse<ObavijestResponse>>(_id.Value, _request);
+                    MessageBox.Show($"Obavijest {txtNaslov.Text} uspješno uređena!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+
+                    _request.Datum = DateTime.Now;
+                    _request.KorisnikId = Properties.Settings.Default.PrijavljeniKorisnik.Id;
+
+                    PayloadResponse<ObavijestResponse> response = await _service.Insert<PayloadResponse<ObavijestResponse>>(_request);
+                    MessageBox.Show($"Obavijest {response.Payload.Naslov} uspješno dodana!", "", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+
+                DialogResult = DialogResult.OK;
+                Close();
+            }
+        }
+
+        private void BtnOcisti_Click(object sender, EventArgs e)
+        {
+            SetValues();
+        }
+
+        //VALIDACIJA
+        private void txtNaslov_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtNaslov.Text.Trim()))
+            {
+                e.Cancel = true;
+                err.SetError(txtNaslov, "Obavezno polje!");
+            }
+            else
+            {
+                err.SetError(txtNaslov, null);
+            }
+        }
+
+        private void txtTekst_Validating(object sender, CancelEventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtTekst.Text.Trim()))
+            {
+                e.Cancel = true;
+                err.SetError(txtTekst, "Obavezno polje!");
+            }
+            else
+            {
+                err.SetError(txtTekst, null);
+            }
+        }
+    }
+}
