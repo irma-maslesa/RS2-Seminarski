2fa43eb857613a9c6e4b12b50e0096e2042be3bb
Irma Masleša <irma.maslesa@edu.fit.ba>
2021-12-31 13:38:57 +0100

    Added filter projekcije by zanr option

--- Pelikula.MOBILE/lib/pages/projekcija/projekcije.dart
+++ Pelikula.MOBILE/lib/pages/projekcija/projekcije.dart
@@ -1,9 +1,8 @@
-import 'dart:convert';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:http/http.dart';
+import 'package:pelikula_mobile/model/lov.dart';
 import 'package:pelikula_mobile/model/projekcija/projekcija_detailed_response.dart';
 import 'package:pelikula_mobile/model/response/error_response.dart';
 import 'package:pelikula_mobile/model/response/paged_payload_response.dart';
@@ -25,19 +24,26 @@ class _ProjekcijeState extends State<Projekcije> {
   TextEditingController nazivController = TextEditingController();
   String? _naziv;
 
+  List<DropdownMenuItem> zanrovi = [
+    DropdownMenuItem(
+        child: Text(
+          "Muško",
+          style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+        ),
+        value: 1),
+    DropdownMenuItem(
+        child: Text(
+          "Žensko",
+          style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+        ),
+        value: 2),
+  ];
+  int? _odabraniZanr = 0;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Projekcije"),
-      ),
-      drawer: const MyDrawer(),
-      body: body(),
-    );
-  }
-
-  Widget body() {
     final txtNaziv = TextFormField(
+      autofocus: false,
       controller: nazivController,
       obscureText: false,
       style: style,
@@ -47,13 +53,10 @@ class _ProjekcijeState extends State<Projekcije> {
         border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
         suffixIcon: IconButton(
           onPressed: () {
-            if (nazivController.text.trim().length > 2) {
-              _naziv = nazivController.text;
-            } else {
-              _naziv = null;
-            }
+            _naziv = nazivController.text == "" ? null : nazivController.text;
+
             setState(() {
-              getProjekcije(_naziv);
+              getProjekcije(_naziv, _odabraniZanr);
             });
           },
           icon: const Icon(Icons.search),
@@ -61,8 +64,22 @@ class _ProjekcijeState extends State<Projekcije> {
       ),
     );
 
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Projekcije"),
+      ),
+      drawer: const MyDrawer(),
+      body: Column(children: [
+        Padding(padding: const EdgeInsets.all(10), child: txtNaziv),
+        Padding(padding: const EdgeInsets.all(10), child: ddZanr()),
+        Expanded(child: body())
+      ]),
+    );
+  }
+
+  Widget body() {
     return FutureBuilder<dynamic>(
-      future: getProjekcije(_naziv),
+      future: getProjekcije(_naziv, _odabraniZanr),
       builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
         if (snapshot.connectionState == ConnectionState.waiting) {
           return const Center(
@@ -72,23 +89,22 @@ class _ProjekcijeState extends State<Projekcije> {
           return const Center(
             child: Text("Greška pri učitavanju."),
           );
-        } else if (snapshot.data is PagedPayloadResponse) {
-          return Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Padding(padding: const EdgeInsets.all(10), child: txtNaziv),
-                Expanded(
-                  child: ListView(
-                    children: (snapshot.data.payload
-                            .map((e) => ProjekcijaDetailedResponse.fromJson(e))
-                            .toList()
-                            .cast<ProjekcijaDetailedResponse>() as List)
-                        .map((e) => projekcijaWidget(e))
-                        .toList()
-                        .cast<Widget>(),
-                  ),
-                )
-              ]);
+        } else if (snapshot.data is PagedPayloadResponse &&
+            snapshot.data.payload.length > 0) {
+          return ListView(
+            children: (snapshot.data.payload
+                    .map((e) => ProjekcijaDetailedResponse.fromJson(e))
+                    .toList()
+                    .cast<ProjekcijaDetailedResponse>() as List)
+                .map((e) => projekcijaWidget(e))
+                .toList()
+                .cast<Widget>(),
+          );
+        } else if (snapshot.data is PagedPayloadResponse &&
+            snapshot.data.payload.length == 0) {
+          return const Center(
+            child: Text("Nema rezultata"),
+          );
         } else {
           return Center(
             child: Text((snapshot.data as ErrorResponse).message as String),
@@ -98,20 +114,20 @@ class _ProjekcijeState extends State<Projekcije> {
     );
   }
 
-  Future<dynamic> getProjekcije(String? naziv) async {
+  Future<dynamic> getProjekcije(String? naziv, int? zanrId) async {
     Map<String, dynamic> params = {};
 
     if (naziv != null && naziv.trim().isNotEmpty) {
       params["naziv"] = naziv;
     }
 
-    dynamic response;
-
-    if (params.isEmpty) {
-      response = await ApiService.get("Projekcija/aktivne/details", null);
-    } else {
-      response = await ApiService.get("Projekcija/aktivne/details", params);
+    if (zanrId != null && zanrId != 0) {
+      params["zanrId"] = zanrId.toString();
     }
+
+    dynamic response = params.isEmpty
+        ? await ApiService.get("Projekcija/aktivne/details", null)
+        : await ApiService.get("Projekcija/aktivne/details", params);
 
     return response;
   }
@@ -161,5 +177,82 @@ class _ProjekcijeState extends State<Projekcije> {
         ),
       ),
     );
+  }
+
+  Widget ddZanr() {
+    return FutureBuilder<dynamic>(
+      future: getZanrove(_odabraniZanr),
+      builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: Text("Učitavanje..."),
+          );
+        } else if (snapshot.hasError) {
+          return const Center(
+            child: Text("Greška pri učitavanju."),
+          );
+        } else if (snapshot.data is PagedPayloadResponse) {
+          return DropdownButtonFormField(
+            onChanged: (dynamic newVal) {
+              setState(() {
+                _odabraniZanr = newVal;
+                getProjekcije(_naziv, _odabraniZanr);
+              });
+            },
+            hint: Text(
+              'Žanr',
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+            ),
+            isExpanded: true,
+            items: zanrovi,
+            value: _odabraniZanr,
+            decoration: InputDecoration(
+              contentPadding: const EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+              border:
+                  OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
+            ),
+          );
+        } else {
+          return Center(
+            child: Text((snapshot.data as ErrorResponse).message as String),
+          );
+        }
+      },
+    );
+  }
+
+  Future<dynamic> getZanrove(int? odabraniZanr) async {
+    dynamic response = await ApiService.get("Zanr/lov", null);
+
+    if (response is PagedPayloadResponse) {
+      zanrovi = (response.payload
+              .map((e) => LoV.fromJson(e))
+              .toList()
+              .cast<LoV>() as List)
+          .map((e) {
+        return DropdownMenuItem(
+            child: Text(
+              e.naziv,
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+            ),
+            value: e.id);
+      }).toList();
+
+      zanrovi = [
+        DropdownMenuItem(
+            child: Text(
+              "Svi žanrovi",
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+            ),
+            value: 0),
+        ...zanrovi
+      ];
+
+      if (odabraniZanr != null && odabraniZanr != 0) {
+        _odabraniZanr = odabraniZanr;
+      }
+    }
+
+    return response;
   }
 }
